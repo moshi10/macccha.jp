1dadbc07f7dfc473dc277312425a6000c8445972
moshi10 <mamiko.oshino@gmail.com>
2022-06-27 14:45:14 +0900

    fix: [id].tsx

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -13,38 +13,38 @@ type Props = {
   blog: Blog
 }
 
-const BlogId:NextPage<Props> = ({ blog }) => {
-    return (
-      <main>
-        <h1>{blog.title}</h1>
-        <p>{blog.publishedAt}</p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `${blog.content}`,
-          }}
-        />
-      </main>
-    );
-  }
-  
-  // 静的生成のためのパスを指定します
-  export const getStaticPaths = async () => {
-    const data = await client.get<MicroCMSListResponse<Blog>>({ endpoint: "blog" });
-  
-    const paths = data.contents.map((content) => `/blog/${content.id}`);
-    return { paths, fallback: false };
+const BlogId: NextPage<Props> = ({ blog }) => {
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.content}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get<MicroCMSListResponse<Blog>>({ endpoint: "blog" });
+
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blogs", contentId: id });
+
+  return {
+    props: {
+      blogs: data,
+    },
   };
-  
-  // データをテンプレートに受け渡す部分の処理を記述します
-  export const getStaticProps = async (context:any) => {
-    const id = context.params.id;
-    const data = await client.get({ endpoint: "blog", contentId: id });
-  
-    return {
-      props: {
-        blog: data,
-      },
-    };
-  };
+};
 
 export default BlogId;
