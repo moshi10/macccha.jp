eb33cb09a3800eca2def1c19bfe731d2e29c0914
oshino <mamiko.oshino@gmail.com>
2023-06-05 01:07:01 +0900

    作り直し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardFooter, Heading, Stack, VStack, Text, Image, Center, CardHeader, StackDivider, Container } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from 'next/head'
 import About from "../components/About";
@@ -15,7 +15,57 @@ const Home: NextPage = () => {
         <meta name="description" content="ﾏｯﾁｬのポートフォリオサイトです。" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <VStack spacing={0} align="center" minH="100vh" m="0 auto">
+      <Box
+        bg="red.50"
+        h="100vh"
+      >
+
+
+        <VStack minW={"md"} p="40px 0 0">
+          <Center>
+            <Image
+              src="matcha.jpg"
+              h="75px"
+              borderRadius='full'
+              alt="Matchaの画像" />
+          </Center>
+          <Heading size='xl'>
+            <Box as="span" textDecoration="underline" textDecorationColor="red.300" textDecorationThickness="10%">
+              macccha
+            </Box>.jp</Heading>
+
+
+          <Container maxW='container.sm' pt={10}>
+            <Box>
+              <Heading size='md' textTransform='uppercase'>
+                業務経歴
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='md' textTransform='uppercase'>
+                個人製作
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='md' textTransform='uppercase'>
+                お問い合せ
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+
+              </Text>
+            </Box>
+          </Container>
+
+        </VStack>
+
+      </Box>
+      {/* <VStack spacing={0} align="center" minH="100vh" m="0 auto">
         <Header />
         <VStack w="100%" minH="calc(100vh - 64px)">
           <Hero />
@@ -23,7 +73,7 @@ const Home: NextPage = () => {
           <About />
         </VStack>
         <Footer />
-      </VStack>
+      </VStack> */}
     </>
 
   );
